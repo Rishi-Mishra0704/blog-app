9d83853ae4ceabe953ab01212d0cbe9d281fa66e
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-06-20 20:44:42 +0530

    Add spec for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
+# Tests Comments
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    user = User.create(name: 'Rishi', photo: 'Image will be displayed here', bio: 'Hello pals', posts_counter: 0)
+    post = Post.create(title: 'First Post', text: 'This is my first post', author_id: user.id, comments_counter: 0,
+                       likes_counter: 0)
+    subject { described_class.create(text: 'This is my first comment', post:, user:) }
+
+    it 'is valid without a text' do
+      subject.text = nil
+      expect(subject).to be_valid
+    end
+
+    it 'posts comments count should increase' do
+      expect(subject.post.comments_counter).to eq(post.comments_counter)
+    end
+  end
 end
