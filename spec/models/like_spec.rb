6bbd998ed75aec86ecd9cfa1a642ad67994ebab2
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-06-20 20:11:13 +0530

    Add specs for like post and user

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  describe 'Validations' do
+    let(:user) do
+      User.create(name: 'Rishi', photo: 'Image will be displayed here', bio: 'Hello guys', posts_counter: 0)
+    end
+    let(:post) do
+      Post.create(title: 'First Post', text: 'This is my first post', author_id: user.id, comments_counter: 0,
+                  likes_counter: 0)
+    end
+
+    subject { described_class.new(post:, user:) }
+
+    it 'must contain user id' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'must contain post id' do
+      subject.post_id = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'increments the likes counter of the associated post' do
+      expect do
+        ActiveRecord::Base.transaction do
+          subject.save
+          post.reload
+        end
+      end.to change { post.likes_counter }.by(1)
+    end
+  end
+end
