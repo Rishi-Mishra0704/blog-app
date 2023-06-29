d8d7fab279eeaa206a8ca8af99a686394c95783d
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-06-29 13:08:23 +0530

    Add a method to make a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,17 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @comments = Comment.where(post_id: params[:id])
   end
+
+  def new
+    @user = @current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = @current_user
+    @post = Post.new(author_id: @user, title: params[:post][:title], text: params[:post][:text])
+    @post.author_id = @user.id
+    @post.save
+    redirect_to user_posts_path(@user)
+  end
 end
