2b45fdab8657923a97ea6f6c68621283b06473aa
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-04 19:13:25 +0600

    Add post index_spec

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
-
-RSpec.describe 'posts/index.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'Post #Index Page', type: :feature do
+  before(:each) do
+    @user = User.create(name: 'Mondol', photo: 'https://example.jpg',
+                        bio: 'He is a software engineer from Bangladesh', posts_counter: 10)
+    @post = Post.create(author: @user, title: 'test post', text: 'This is my first post', likes_counter: 15,
+                        comments_counter: 10)
+    @comment = Comment.create(post_id: @post.id, user_id: @user.id, text: 'test comment')
+    visit user_posts_path(@user)
+  end
 end
