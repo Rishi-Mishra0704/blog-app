a37312926fb31a165e8e2e12a78b93e5b70ca048
Abdus Sattar <sasorganicagro@gmail.com>
2023-07-05 13:55:24 +0600

    Add devise route and scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'sessions'
-  }
-
+  devise_for :users
   root "users#index"
+
+  devise_scope :user do
+    get 'login', to 'devise/sessions#new'
+    get 'sign_up', to 'devise/registrations#new'
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
